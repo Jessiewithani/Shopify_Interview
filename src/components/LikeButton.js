beee9153ca81478f54a56d3b68bad225d0a563d5
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-09-13 15:58:16 -0600

    Create LikeButton Component and styles

---
+++ src/components/LikeButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/LikeButton.css';
+
+function LikeButton () {
+  return (
+      <button className="like-btn">LIKE</button>
+  )
+}
+
+export default LikeButton;
--- src/components/PostsContainer.js
+++ src/components/PostsContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/PostsContainer.css';
+import Post from '../components/Post';
 
 function PostsContainer () {
   return (
     <div className="container">
       POSTS CONTAINER
+      <Post/>
     </div>
   )
 }
