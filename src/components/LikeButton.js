6de447d739445f502ebc6020f7005d3c87722af1
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-09-14 16:31:09 -0600

    Style post card
    to look more presentable

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -4,16 +4,24 @@ import rocket from '../images/rocket.svg'
 
 function LikeButton () {
 
+  const rocketIcon = document.getElementsByClassName("like-icon")[0]
+
   const [toggleLike, setToggleLike] = useState(true)
 
   const handleLikeClick = () => {
     setToggleLike(!toggleLike)
+
   }
 
+  
+
   return (
-    <div>
-      <button className="like-btn" onClick={handleLikeClick}>{toggleLike ? "LIKE" : "UNLIKE"}</button>
-      {/* <img src={rocket} className="like-icon" alt="rocket" /> */}
+    <div className="btn-section">
+      {console.log('rock', rocketIcon)}
+      <div className="icon-cont" >
+        <img src={rocket} className={`like-icon ${!toggleLike ? 'liked' : null}`} alt="rocket" onClick={handleLikeClick}/>
+      {/* <button className="like-btn" onClick={handleLikeClick}>{toggleLike ? "LIKE" : "UNLIKE"}</button> */}
+      </div>
     </div>
   )
 }
