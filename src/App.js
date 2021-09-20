19d34fcba2ee1dbd72e3b20b1ff90f27ebed1d87
Jessie Le-Ho <lehoj@oregonstate.edu>
2021-09-19 19:56:34 -0600

    Make navbar sticky

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import Loader from './components/Loader';
 
 function App() {
 
-  
   const [spaceImages, setSpaceImages] = useState(null)
   const [loading, setLoading] = useState(false)
   
-
   useEffect(async () => {
     const response = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=UF4Alv0TEPHcf6ZgVFV2sJ4eE2QqcFghTpAqyQ7M')
     const results = await response.json();
@@ -27,7 +25,6 @@ function App() {
     <div className="App">
       <Navbar/>
       {!spaceImages ? <Loader loading={loading} images={spaceImages}/> : <PostsContainer images={spaceImages}/>}
-      {/* <PostsContainer images={spaceImages}/> */}
     </div>
   );
 }
